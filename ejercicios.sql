7db5157246d137f07e009deab00ba65d5684e340
lucianomolinas <lucianomolinas74@gmail.com>
2025-12-18 19:03:14 -0300

    version 1

---
+++ ejercicios.sql
@@ -0,0 +1,14 @@
+-- ejercicio 1 
+
+CREATE DATABASE veterinaria_patitas_felices; 
+
+-- ejercicio 2 
+
+use veterinaria_patitas_felices;
+
+CREATE TABLE dueños (
+  
+id_dueños INT AUTO_INCREMENT PRIMARY KEY , 
+
+
+);
